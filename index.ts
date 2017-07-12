544b8b319a91c8dacd7126aea36cacb226503a50
1ven <1ven.mironchik@gmail.com>
2017-07-12 13:18:55 +0300

    Add Profunctor

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ export * from './Functor';
 export * from './Monoid';
 export * from './Ord';
 export * from './Plus';
+export * from './Profunctor';
 export * from './Semigroup';
 export * from './Semigroupoid';
 export * from './Setoid';
