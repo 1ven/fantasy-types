39a228883cffb831204db83bb9f4be0784ac2ebe
1ven <1ven.mironchik@gmail.com>
2017-07-14 14:26:55 +0300

    Add MonadConstructor

--- Monad.ts
+++ Monad.ts
@@ -1,4 +1,8 @@
-import { Applicative } from "./Applicative";
+import { Applicative, ApplicativeConstructor } from "./Applicative";
 import { Chain } from "./Chain";
 
 export interface Monad<T> extends Applicative<T>, Chain<T> {}
+
+export interface MonadConstructor extends ApplicativeConstructor {
+  of: <T1>(a: T1) => Monad<T1>;
+}
