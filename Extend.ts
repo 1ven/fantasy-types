9605a028fb3b689063a4687152903b5163e3c257
1ven <1ven.mironchik@gmail.com>
2017-07-14 14:26:41 +0300

    Fix Extend

--- Extend.ts
+++ Extend.ts
@@ -1,5 +1,5 @@
 import { Functor } from "./Functor";
 
 export interface Extend<T> extends Functor<T> {
-  extend: <T1>(f: (a: Extend<T>) => T1) => Extend<T1>;
+  extend: <T1>(f: (a: Extend<T>) => T1) => Extend<Extend<T1>>;
 }
