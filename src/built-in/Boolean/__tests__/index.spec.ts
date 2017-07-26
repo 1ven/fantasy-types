b1ed9fc30bdfa427086fe5a6376eef030bff816f
1ven <1ven.mironchik@gmail.com>
2017-07-26 18:00:49 +0300

    Refactor Boolean tests

--- src/built-in/Boolean/__tests__/index.spec.ts
+++ src/built-in/Boolean/__tests__/index.spec.ts
@@ -1,34 +1,25 @@
-import BooleanExtended from "../";
 import { laws } from "../../../types";
 
 test("conforms Setoid reflexivity", () => {
-  laws.Setoid.reflexivity(new BooleanExtended(true));
+  laws.Setoid.reflexivity(true);
 });
 
 test("conforms Setoid symmetry", () => {
-  laws.Setoid.symmetry(new BooleanExtended(true), new BooleanExtended(false));
+  laws.Setoid.symmetry(true, false);
 });
 
 test("conforms Setoid transitivity", () => {
-  laws.Setoid.transitivity(
-    new BooleanExtended(true),
-    new BooleanExtended(true),
-    new BooleanExtended(true)
-  );
+  laws.Setoid.transitivity(true, true, true);
 });
 
 test("conforms Ord totality", () => {
-  laws.Ord.totality(new BooleanExtended(true), new BooleanExtended(false));
+  laws.Ord.totality(true, false);
 });
 
 test("conforms Ord antisymmetry", () => {
-  laws.Ord.antisymmetry(new BooleanExtended(true), new BooleanExtended(true));
+  laws.Ord.antisymmetry(true, true);
 });
 
 test("conforms Ord transitivity", () => {
-  laws.Ord.transitivity(
-    new BooleanExtended(false),
-    new BooleanExtended(false),
-    new BooleanExtended(false)
-  );
+  laws.Ord.transitivity(false, false, false);
 });
