ad421db89f59ce9bd0212106e732d57c47a598b9
1ven <1ven.mironchik@gmail.com>
2017-07-26 21:52:32 +0300

    Refactor String tests

--- src/built-in/RegExp/__tests__/index.spec.ts
+++ src/built-in/RegExp/__tests__/index.spec.ts
@@ -1,18 +1,13 @@
-import RegExpExtended from "../";
 import { laws } from "../../../types";
 
 test("conforms Setoid reflexivity", () => {
-  laws.Setoid.reflexivity(new RegExpExtended("a"));
+  laws.Setoid.reflexivity(new RegExp("a"));
 });
 
 test("conforms Setoid symmetry", () => {
-  laws.Setoid.symmetry(new RegExpExtended("a"), new RegExpExtended("b"));
+  laws.Setoid.symmetry(new RegExp("a"), new RegExp("b"));
 });
 
 test("conforms Setoid transitivity", () => {
-  laws.Setoid.transitivity(
-    new RegExpExtended("a"),
-    new RegExpExtended("a"),
-    new RegExpExtended("a")
-  );
+  laws.Setoid.transitivity(new RegExp("a"), new RegExp("a"), new RegExp("a"));
 });
--- src/built-in/String/__tests__/index.spec.ts
+++ src/built-in/String/__tests__/index.spec.ts
@@ -1,50 +1,37 @@
-import StringExtended from "../";
 import { laws } from "../../../types";
 
 test("conforms Setoid reflexivity", () => {
-  laws.Setoid.reflexivity(new StringExtended("a"));
+  laws.Setoid.reflexivity("a");
 });
 
 test("conforms Setoid symmetry", () => {
-  laws.Setoid.symmetry(new StringExtended("a"), new StringExtended("a"));
+  laws.Setoid.symmetry("a", "a");
 });
 
 test("conforms Setoid transitivity", () => {
-  laws.Setoid.transitivity(
-    new StringExtended("a"),
-    new StringExtended("a"),
-    new StringExtended("a")
-  );
+  laws.Setoid.transitivity("a", "a", "a");
 });
 
 test("conforms Ord totality", () => {
-  laws.Ord.totality(new StringExtended("a"), new StringExtended("b"));
+  laws.Ord.totality("a", "b");
 });
 
 test("conforms Ord antisymmetry", () => {
-  laws.Ord.antisymmetry(new StringExtended("a"), new StringExtended("a"));
+  laws.Ord.antisymmetry("a", "a");
 });
 
 test("conforms Ord transitivity", () => {
-  laws.Ord.transitivity(
-    new StringExtended("a"),
-    new StringExtended("b"),
-    new StringExtended("c")
-  );
+  laws.Ord.transitivity("a", "b", "c");
 });
 
 test("conforms Semigroup associativity", () => {
-  laws.Semigroup.associativity(
-    new StringExtended("a"),
-    new StringExtended("b"),
-    new StringExtended("c")
-  );
+  laws.Semigroup.associativity("a", "b", "c");
 });
 
 test("conforms Monoid right identity", () => {
-  laws.Monoid.rightIdentity(StringExtended, new StringExtended("m"));
+  laws.Monoid.rightIdentity(String, "m");
 });
 
 test("conforms Monoid left identity", () => {
-  laws.Monoid.leftIdentity(StringExtended, new StringExtended("m"));
+  laws.Monoid.leftIdentity(String, "m");
 });
