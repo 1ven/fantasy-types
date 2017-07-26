0f3722e87126ef5a36ad271d7031f765f00626a2
1ven <1ven.mironchik@gmail.com>
2017-07-26 10:43:29 +0300

    Add Function type index file

---
+++ src/built-in/Function/index.ts
@@ -0,0 +1,51 @@
+import * as F from "../../types";
+import { extendClass } from "../../utils";
+
+import id from "./id";
+import of from "./of";
+import equals from "./equals";
+import compose from "./compose";
+import map from "./map";
+import promap from "./promap";
+import ap from "./ap";
+import chain from "./chain";
+import contramap from "./contramap";
+
+export interface FunctionExtended<T>
+  extends F.Functor<T>,
+    F.Profunctor<T>,
+    F.Apply<T>,
+    F.Applicative<T>,
+    F.Chain<T>,
+    F.Contravariant<T>,
+    F.Setoid,
+    F.Semigroupoid,
+    F.Category {}
+
+export interface Constructor
+  extends F.ApplicativeConstructor,
+    F.CategoryConstructor {
+  new (...args: Array<string>);
+}
+
+export const is = (val: any): val is Function => typeof val === "function";
+export const isConstructor = (Ctor: any): Ctor is FunctionConstructor =>
+  Ctor === Function;
+
+export const extended = {
+  id,
+  of,
+  prototype: {
+    equals,
+    compose,
+    map,
+    promap,
+    ap,
+    chain,
+    contramap
+  }
+};
+
+const FunctionExtended: Constructor = extendClass(Function, extended);
+
+export default FunctionExtended;
