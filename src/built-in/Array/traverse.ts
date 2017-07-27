65e25368e073fd89c1a9f3e821a20d20148f7f3c
1ven <1ven.mironchik@gmail.com>
2017-07-27 16:05:47 +0300

    Refactor Array traverse

--- src/built-in/Array/traverse.ts
+++ src/built-in/Array/traverse.ts
@@ -1,5 +1,12 @@
 import { curry } from "../../methods";
-import { Applicative, ApplicativeConstructor, of, map, ap } from "../../types";
+import {
+  Applicative,
+  ApplicativeConstructor,
+  of,
+  map,
+  ap,
+  concat
+} from "../../types";
 
 export default curry(
   <T, T1>(
@@ -7,16 +14,10 @@ export default curry(
     f: (a: T) => Applicative<T1>,
     arr: T[]
   ) => {
-    type A = Applicative<T1>;
-    type L = Array<A>;
-
-    const list = map(f, arr);
     let acc = of(A, []);
-    let idx = list.length - 1;
 
-    while (idx >= 0) {
-      acc = ap(map((el: A) => (list: L) => [el, ...list], list[idx]), acc);
-      idx -= 1;
+    for (let v of arr) {
+      acc = ap(map(v => list => concat(list, [v]), f(v)) as any, acc);
     }
 
     return acc;
