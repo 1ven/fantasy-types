cc9bef42c2a5e2ad26b2f449ecc02e1957ea5cef
1ven <1ven.mironchik@gmail.com>
2017-07-27 14:12:34 +0300

    Change imports

--- src/built-in/Function/__tests__/index.spec.ts
+++ src/built-in/Function/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import * as F from "../../../types";
+import { laws } from "../../../types";
 
 const exec = (x, input) => {
   const result = x(input);
@@ -14,24 +14,24 @@ const run = (law, args, input) =>
   ]);
 
 test("obeys Functor identity", () => {
-  run(F.laws.Functor.identity, [a => a + 1], 5);
+  run(laws.Functor.identity, [a => a + 1], 5);
 });
 
 test("obeys Functor composition", () => {
   run(
-    F.laws.Functor.composition,
+    laws.Functor.composition,
     [x => x + "a", x => x + "f", x => x + "g"],
     "x"
   );
 });
 
 test("obeys Profunctor identity", () => {
-  run(F.laws.Profunctor.identity, [x => x + "a"], "x");
+  run(laws.Profunctor.identity, [x => x + "a"], "x");
 });
 
 test("obeys Profunctor composition", () => {
   run(
-    F.laws.Profunctor.composition,
+    laws.Profunctor.composition,
     [x => x + "a", x => x + "f", x => x + "g", x => x + "h", x => x + "i"],
     "x"
   );
@@ -39,69 +39,69 @@ test("obeys Profunctor composition", () => {
 
 test("obeys Apply composition", () => {
   run(
-    F.laws.Apply.composition,
+    laws.Apply.composition,
     [x => x + "a", x => y => x + y + "u", x => y => x + y + "a"],
     "x"
   );
 });
 
 test("obeys Applicative identity", () => {
-  run(F.laws.Applicative.identity, [Function, x => x + "a"], "x");
+  run(laws.Applicative.identity, [Function, x => x + "a"], "x");
 });
 
 test("obeys Applicative homomorphism", () => {
-  run(F.laws.Applicative.homomorphism, [Function, "val", x => x + "b"], "x");
+  run(laws.Applicative.homomorphism, [Function, "val", x => x + "b"], "x");
 });
 
 test("obeys Applicative interchange", () => {
-  run(F.laws.Applicative.interchange, [Function, "val", x => y => x + y], "x");
+  run(laws.Applicative.interchange, [Function, "val", x => y => x + y], "x");
 });
 
 test("obeys Chain associativity", () => {
   run(
-    F.laws.Chain.associativity,
+    laws.Chain.associativity,
     [x => x + "a", x => y => x + y + "f", x => y => x + y + "g"],
     "x"
   );
 });
 
 test("obeys Contravariant identity", () => {
-  run(F.laws.Contravariant.identity, [x => y => x + y + "a"], "x");
+  run(laws.Contravariant.identity, [x => y => x + y + "a"], "x");
 });
 
 test("obeys Contravariant composition", () => {
   run(
-    F.laws.Contravariant.composition,
+    laws.Contravariant.composition,
     [x => x + "a", x => x + "f", x => x + "g"],
     "x"
   );
 });
 
 test("obeys Setoid reflexivity", () => {
-  F.laws.Setoid.reflexivity(x => x + "a");
+  laws.Setoid.reflexivity(x => x + "a");
 });
 
 test("obeys Setoid symmetry", () => {
-  F.laws.Setoid.symmetry(x => x + "a", y => y + "a");
+  laws.Setoid.symmetry(x => x + "a", y => y + "a");
 });
 
 test("obeys Setoid transitivity", () => {
   const func = x => x + "a";
-  F.laws.Setoid.transitivity(func, func, func);
+  laws.Setoid.transitivity(func, func, func);
 });
 
 test("obeys Semigroupoid associativity", () => {
   run(
-    F.laws.Semigroupoid.associativity,
+    laws.Semigroupoid.associativity,
     [x => x + "a", x => x + "b", x => x + "c"],
     "x"
   );
 });
 
 test("obeys Category left identity", () => {
-  run(F.laws.Category.leftIdentity, [Function, x => x + "a"], "x");
+  run(laws.Category.leftIdentity, [Function, x => x + "a"], "x");
 });
 
 test("obeys Category right identity", () => {
-  run(F.laws.Category.rightIdentity, [Function, x => x + "a"], "x");
+  run(laws.Category.rightIdentity, [Function, x => x + "a"], "x");
 });
--- src/types/Profunctor/laws.ts
+++ src/types/Profunctor/laws.ts
@@ -1,11 +1,11 @@
-import * as F from "../../types";
+import { promap } from "../../types";
 
 export const identity = (p, exp = expect) => {
-  exp(F.promap(a => a, b => b, p)).toEqual(p);
+  exp(promap(a => a, b => b, p)).toEqual(p);
 };
 
 export const composition = (p, f, g, h, i, exp = expect) => {
-  exp(F.promap(a => f(g(a)), b => h(i(b)), p)).toEqual(
-    F.promap(g, h, F.promap(f, i, p))
+  exp(promap(a => f(g(a)), b => h(i(b)), p)).toEqual(
+    promap(g, h, promap(f, i, p))
   );
 };
